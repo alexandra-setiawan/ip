7082f4547cfc34aba39cfb9768ffcfa6845f8412
Alexandra Setiawan <alexandra.m.setiawan@gmail.com>
2026-08-19 22:22:42 +0800

    Level-0: Update initial greeting and exit behavior

--- src/main/java/Edith.java
+++ src/main/java/Edith.java
@@ -5,6 +5,14 @@ public class Edith {
                       + "|  _| / _` | | __| '_ \\ \n"
                       + "| |__| (_| | | |_| | | |\n"
                       + "|_____\\__,_|_|\\__|_| |_|\n";
+        String line = "____________________________________________________________";
+
+        System.out.println(line);
         System.out.println(banner);
+        System.out.println("Hello! I'm Edith.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
