31b1c40cab36a69097fef65a1a181f27c7b721f2
Alexandra Setiawan <alexandra.m.setiawan@gmail.com>
2026-08-19 22:35:16 +0800

    Level-1: Implement echo capability and bye exit command

--- src/main/java/Edith.java
+++ src/main/java/Edith.java
@@ -1,18 +1,44 @@
+import java.util.Scanner;
+
+/**
+ * Edith is a chatbot that echoes user commands until the user says goodbye.
+ */
+
 public class Edith {
+    /**
+     * Starts the chatbot.
+     * 
+     * @param args command line arguments (not used)
+     */
     public static void main(String[] args) {
         String banner = " _____    _ _ _   _     \n"
                       + "| ____|__| (_) |_| |__  \n"
                       + "|  _| / _` | | __| '_ \\ \n"
                       + "| |__| (_| | | |_| | | |\n"
                       + "|_____\\__,_|_|\\__|_| |_|\n";
-        String line = "____________________________________________________________";
+        String line = "____________________________________________________";
 
         System.out.println(line);
         System.out.println(banner);
-        System.out.println("Hello! I'm Edith.");
-        System.out.println("What can I do for you?");
+        System.out.println("\tHello! I'm Edith.");
+        System.out.println("\tWhat can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+
+        try (Scanner scanner = new Scanner(System.in)) {            
+            while (true) {
+                String command = scanner.nextLine();
+                
+                System.out.println(line);
+
+                if (command.equals("bye")) {
+                    System.out.println("\tBye. Hope to see you again soon!");
+                    System.out.println(line);
+                    break;
+                }
+
+                System.out.println("\t" + command + " " + command + " " + command);
+                System.out.println(line);
+            }
+        }
     }
 }
