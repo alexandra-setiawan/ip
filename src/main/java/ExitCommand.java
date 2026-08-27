7d98906146f08b208e6e44e1b5ad66efb638a283
Alexandra Setiawan <alexandra.m.setiawan@gmail.com>
2026-08-27 23:28:43 +0800

    A-MoreOOP

---
+++ src/main/java/ExitCommand.java
@@ -0,0 +1,5 @@
+/** Ends the Edith session. */
+public class ExitCommand extends Command {
+    @Override public void execute(TaskList tasks, Ui ui, Storage storage) { ui.showBye(); }
+    @Override public boolean isExit() { return true; }
+}
